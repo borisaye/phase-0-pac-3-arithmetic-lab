f4cc6145eeec22a2032cf18e39a56529f7a57f64
Borisaye <Boris_aroche@hotmail.com>
2023-03-05 11:28:37 -0500

    add

---
+++ index.js
@@ -0,0 +1,60 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function add(a, b){
+    return a + b;
+
+}
+
+function subtract(a, b){
+    return a - b;
+
+}
+
+function multiply(a, b){
+    return a * b;
+
+}
+
+function divide(a, b){
+    return a / b;
+
+}
+
+function increment(n){
+    return ++n;
+
+}
+
+function decrement(n){
+    return --n;
+
+}
+
+function makeInt(n, radix){
+    return parseInt(n);
+
+}
+
+function makeInt(n, radix){
+    return parseFloat(n);
+
+}
+
+function preserveDecimal(n, radix){
+    return parseFloat(n);
+
+}
